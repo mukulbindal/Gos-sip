30ed8e396c8747cf6da01c4723afa26b6d39f499
mukulbindal <mukulbindal170299@gmail.com>
2023-01-23 18:39:31 +0530

    Initial Commit

---
+++ backend/models/userModel.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        pic:{
+            type: String,
+            required: false,
+            default: "",
+        }
+    },{
+        timestamps: true,
+    });
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const chats = require('./data/data')
+dotenv.config();
+const app = express();
+
+app.get("/api/chat", (req, res)=>{
+    res.send(chats);
+});
+
+app.get("/api/chat/:id", (req, res)=>{
+    const singleChat = chats.find(c=> c._id===req.params.id);
+    res.send(singleChat);
+})
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server started on PORT ${PORT}`))
