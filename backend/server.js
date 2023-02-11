694a6d24be4c61bde3953d490873d9028f5d8e63
mukulbindal <mukulbindal170299@gmail.com>
2023-02-11 15:44:03 +0530

    Modified for aws

--- backend/server.js
+++ backend/server.js
@@ -56,23 +56,25 @@ const PORT = process.env.PORT || 8080;
 // };
 // Start the express App
 var httpsServer;
-
-// httpsServer = https
-//   .createServer(options, app)
-//   .listen(
-//     PORT,
-//     console.log(
-//       `Server started on PORT ${PORT} in ${process.env.NODE_ENV} environment`
-//         .yellow.underline.bold
-//     )
-//   );
-httpsServer = app.listen(
-  PORT,
-  console.log(
-    `Server started on PORT ${PORT} in ${process.env.NODE_ENV} environment`
-      .yellow.underline.bold
-  )
-);
+if (process.env.NODE_ENV === "production") {
+  httpsServer = https
+    .createServer(options, app)
+    .listen(
+      PORT,
+      console.log(
+        `Server started on PORT ${PORT} in ${process.env.NODE_ENV} environment`
+          .yellow.underline.bold
+      )
+    );
+} else {
+  httpsServer = app.listen(
+    PORT,
+    console.log(
+      `Server started on PORT ${PORT} in ${process.env.NODE_ENV} environment`
+        .yellow.underline.bold
+    )
+  );
+}
 
 const io = socketIO(httpsServer, {
   pingTimeout: 60000,
