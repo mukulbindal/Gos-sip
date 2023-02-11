988b310f81b6ed1179b03343e64e2a0070414bb3
mukulbindal <mukulbindal170299@gmail.com>
2023-02-11 15:38:25 +0530

    Modified

--- aws/setup.sh
+++ aws/setup.sh
@@ -1,1 +1,3 @@
-sudo yum install git
+ ssh -i "OwlboxEc2.pem" ec2-user@ec2-54-146-78-115.compute-1.amazonaws.com
+ sudo yum install git
+ sudo yum install npm
---
+++ start.sh
@@ -0,0 +1,2 @@
+export NODE_ENV=production
+node backend/server.js
